13c29ef49ca995c446027649a49c4cecc01b4583
rania-almughem <rania.almughem@gmail.com>
2023-10-05 12:18:36 +0300

    Initial Commit

---
+++ b/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  b
+//
+//  Created by rania on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ b/bApp.swift
@@ -0,0 +1,17 @@
+//
+//  bApp.swift
+//  b
+//
+//  Created by rania on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct bApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
